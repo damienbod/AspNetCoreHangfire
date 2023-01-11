7104a101701e5ba4a1c92670b3e04655067222a6
damienbod <damien_bod@hotmail.com>
2023-01-11 13:18:51 +0100

    Update SQL

--- AspNetCoreHangfire/AspNetCoreHangfire/Program.cs
+++ AspNetCoreHangfire/AspNetCoreHangfire/Program.cs
@@ -1,34 +1,41 @@
-namespace AspNetCoreHangfire
+using Hangfire;
+
+namespace AspNetCoreHangfire;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        var services = builder.Services;
+        var configuration = builder.Configuration;
+        var env = builder.Environment;
+
+        services.AddRazorPages();
+        services.AddHangfire(
+            x => x.UseSqlServerStorage(configuration.GetConnectionString("HangfireConn"))
+        );
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            app.Run();
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        app.Run();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# AspNetCoreHangfire
+# ASP.NET Core Hangfire
+
+## Links
+
+https://jonhilton.net/simple-background-jobs-with-hangfire-and-aspnet-core/
